e7b719bfa7fb3a898b6e932b9ff7dfbbe99be7a6
Ricky <rickywolf28@yahoo.com>
2019-09-08 11:20:26 -0700

    this commit will add random search

--- app.py
+++ app.py
@@ -2,12 +2,15 @@ from flask import Flask, render_template, request
 import os
 import requests
 import json
-import urllib.request,urllib.parse,urllib.error
+import urllib.request
+import urllib.parse
+import urllib.error
 from dotenv import load_dotenv
 
 app = Flask(__name__)
 
-APP_ROOT = os.path.join(os.path.dirname(__file__), './')   # refers to application_top
+# refers to application_top
+APP_ROOT = os.path.join(os.path.dirname(__file__), './')
 dotenv_path = os.path.join(APP_ROOT, '.env')
 load_dotenv(dotenv_path)
 
@@ -16,9 +19,10 @@ api_consumer_key = os.getenv('TENOR_API_KEY')
 # continue a similar pattern until the user makes a selection or starts a new search.
 query = 'panda'
 
+
 @app.route('/')
 def index():
-    urls=[]
+    urls = []
     """Return homepage."""
     # TODO: Extract query term from url
     # query = request.args.get('query')
@@ -34,43 +38,47 @@ def index():
 
     json_data = r.json()
     for i in range(len(json_data['results'])):
-        url = json_data['results'][i]['media'][0]['gif']['url'] #This is the url from json.
+        # This is the url from json.
+        url = json_data['results'][i]['media'][0]['gif']['url']
         urls.append(url)
-        urllib.request.urlretrieve(url, str(i)+'.gif') #Downloads the gif file.
+        # Downloads the gif file.
+        urllib.request.urlretrieve(url, str(i)+'.gif')
     print(urls)
 
     # TODO: Render the 'index.html' template, passing the gifs as a named parameter
 
     return render_template("index.html")
 
+
 @app.route('/trending')
 def index():
-    urls=[]
+    urls = []
     """Return homepage."""
     # TODO: Extract query term from url
     # query = request.args.get('query')
 
-    # TODO: Make 'params' dict with query term and API key
     params = {
-        "query": query,
         "apikey": api_consumer_key
     }
     # TODO: Make an API call to Tenor using the 'requests' library
     r = requests.get(
-        "https://api.tenor.com/v1/trending?q=%s&key=%s&limit=%s" % (params.get('query'), params.get('apikey'), 10))
+        "https://api.tenor.com/v1/trending?q=%s&key=%s&limit=%s" % (params.get('apikey'), 10))
 
     json_data = r.json()
     for i in range(len(json_data['results'])):
-        url = json_data['results'][i]['media'][0]['gif']['url'] #This is the url from json.
+        # This is the url from json.
+        url = json_data['results'][i]['media'][0]['gif']['url']
         urls.append(url)
-        urllib.request.urlretrieve(url, str(i)+'.gif') #Downloads the gif file.
+        # Downloads the gif file.
+        urllib.request.urlretrieve(url, str(i)+'.gif')
     print(urls)
 
     return render_template("index.html")
+
 
 @app.route('/random')
 def index():
-    urls=[]
+    urls = []
     """Return homepage."""
     # TODO: Extract query term from url
     # query = request.args.get('query')
@@ -82,15 +90,19 @@ def index():
     }
     # TODO: Make an API call to Tenor using the 'requests' library
     r = requests.get(
-        "https://api.tenor.com/v1/random?q=%s&key=%s&limit=%s" % (params.get('query'), params.get('apikey'), 10))
+        "https://api.tenor.com/v1/random?q=%s&key=%s&limit=%s" % (params.get('apikey'), 10))
 
     json_data = r.json()
     for i in range(len(json_data['results'])):
-        url = json_data['results'][i]['media'][0]['gif']['url'] #This is the url from json.
+        # This is the url from json.
+        url = json_data['results'][i]['media'][0]['gif']['url']
         urls.append(url)
-        urllib.request.urlretrieve(url, str(i)+'.gif') #Downloads the gif file.
+        # Downloads the gif file.
+        urllib.request.urlretrieve(url, str(i)+'.gif')
     print(urls)
 
     return render_template("index.html")
+
+
 if __name__ == '__main__':
     app.run(debug=True)
