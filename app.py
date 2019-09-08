19ac88cd0b47d3604eaca2b918e9a05ade7e4d1d
Ricky <rickywolf28@yahoo.com>
2019-09-08 11:15:30 -0700

    this commit will add trending search

--- app.py
+++ app.py
@@ -51,6 +51,39 @@ def index():
 
     return render_template("index.html")
 
+@app.route('/trending')
+def index():
+    urls=[]
+    """Return homepage."""
+    # TODO: Extract query term from url
+    # query = request.args.get('query')
 
+    # TODO: Make 'params' dict with query term and API key
+    params = {
+        "query": query,
+        "apikey": api_consumer_key
+    }
+    # TODO: Make an API call to Tenor using the 'requests' library
+    r = requests.get(
+        "https://api.tenor.com/v1/trending?q=%s&key=%s&limit=%s" % (params.get('query'), params.get('apikey'), 10))
+
+    json_data = r.json()
+    for i in range(len(json_data['results'])):
+        url = json_data['results'][i]['media'][0]['gif']['url'] #This is the url from json.
+        urls.append(url)
+        urllib.request.urlretrieve(url, str(i)+'.gif') #Downloads the gif file.
+    print(urls)
+
+
+        # load the GIFs using the urls for the smaller GIF sizes
+
+    # else:
+    #     data = None
+
+    # TODO: Get the first 10 results from the search results
+
+    # TODO: Render the 'index.html' template, passing the gifs as a named parameter
+
+    return render_template("index.html")
 if __name__ == '__main__':
     app.run(debug=True)
