9bdf422c1e5a5b691674fe50ed3ac66c04d9c0cf
Ricky <rickywolf28@yahoo.com>
2019-09-08 11:07:49 -0700

    this commit will add dotenv security

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 from flask import Flask, render_template, request
+import os
 import requests
 import json
 import urllib.request,urllib.parse,urllib.error
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 
-# set the apikey and limit
-api_key = "IPZD64OZGMYV"  # test value
+APP_ROOT = os.path.join(os.path.dirname(__file__), './')   # refers to application_top
+dotenv_path = os.path.join(APP_ROOT, '.env')
+load_dotenv(dotenv_path)
 
-# our test search
-query = "panda"
+api_consumer_key = os.getenv('TENOR_API_KEY')
 
 # continue a similar pattern until the user makes a selection or starts a new search.
-
+query = 'panda'
 
 @app.route('/')
 def index():
@@ -24,7 +26,7 @@ def index():
     # TODO: Make 'params' dict with query term and API key
     params = {
         "query": query,
-        "apikey": api_key
+        "apikey": api_consumer_key
     }
     # TODO: Make an API call to Tenor using the 'requests' library
     r = requests.get(
